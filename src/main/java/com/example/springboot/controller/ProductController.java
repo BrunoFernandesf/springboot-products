ae5d9c8f3e8770c771f9485b35fa4b48688b4401
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-05-09 14:12:04 -0300

    HATEOAS getOneProduct

--- src/main/java/com/example/springboot/controller/ProductController.java
+++ src/main/java/com/example/springboot/controller/ProductController.java
@@ -48,6 +48,7 @@ public class ProductController {
         if(productO.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
         }
+        productO.get().add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
         return ResponseEntity.status(HttpStatus.OK).body(productO.get());
     }
 
